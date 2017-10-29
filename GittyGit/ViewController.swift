a42babee928feb165d623c3ed4e2f5d6eafa8e6c
zenbatista <harald.batista@yahoo.com>
2017-10-28 21:22:25 -0700

    Added comment

--- GittyGit/ViewController.swift
+++ GittyGit/ViewController.swift
@@ -13,12 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        ??nn
+     
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        //Whoa
     }
 
 
